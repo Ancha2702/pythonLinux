66d8c066817893119de395c33d42e040ebf57fde
Ancha2702 <ancha.ru@mail.ru>
2023-09-20 12:22:11 +0300

    4home

---
+++ work4/test_negative2.py
@@ -0,0 +1,24 @@
+import pytest
+
+from sshcheckers import ssh_checkout_negative
+import yaml
+
+with open('config.yaml') as fy:
+    data = yaml.safe_load(fy)
+
+
+class TestNegative:
+    def test_negative1(self, make_folder, clear_folder, make_files, create_bad_archive):  # e извлекли из архива
+
+        assert ssh_checkout_negative(data["host"], data["user"], data["passwd"],
+                                     f'cd {data["folder_bad"]}; 7z e arx2.{data["exten"]} -o{data["folder_ext"]} -y',
+                                     "ERROR")
+
+    def test_negative2(self, make_folder, clear_folder, make_files,
+                       create_bad_archive):  # t проверка целостности архива
+        assert ssh_checkout_negative(data["host"], data["user"], data["passwd"],
+                                     f'cd {data["folder_bad"]}; 7z t arx2.{data["exten"]}', "ERROR")
+
+
+if __name__ == '__main__':
+    pytest.main(['-v'])
